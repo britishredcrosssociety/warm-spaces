ff10869d881b630118860622ee3b3326c25c8abb
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-11-14 15:12:14 +0100

    Remove debug observer

--- R/appServer.R
+++ R/appServer.R
@@ -3,10 +3,6 @@ server <- function(input, output, session) {
   # Global value to track state between map and table
   selected <- reactiveValues(area = vector())
 
-  # observe({
-  #   print(selected$area)
-  # })
-
   mapServer("map", selected = selected)
   tableServer("table", selected = selected)
 }
