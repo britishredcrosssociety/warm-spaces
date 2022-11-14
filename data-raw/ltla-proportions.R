4a6e5a812edc4c8aa0a6d40e4b8288d0a1cec9d4
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-11-14 13:24:34 +0100

    Add updated data to app

---
+++ data-raw/ltla-proportions.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+# Data from ad-hoc/analysis/cost-of-living
+ltla_within_distance <-
+  read_rds("data-raw/imported-data/warm-spaces-ltla-need.rds") |>
+  select(
+    ltla20_code,
+    ltla20_name,
+    region20_name,
+    prop_within_30_min = proportion
+  )
+
+ltla_outside_distance <-
+  read_rds("data-raw/imported-data/warm-spaces-ltla-unmet-need.rds") |>
+  select(
+    ltla20_code,
+    ltla20_name,
+    region20_name,
+    prop_outside_30_min = proportion
+  )
+
+ltla_proportions <- ltla_outside_distance |>
+  left_join(ltla_within_distance) |>
+  mutate(
+    prop_within_30_min = if_else(
+      is.na(prop_within_30_min),
+      0,
+      prop_within_30_min
+    )
+  )
+
+usethis::use_data(ltla_proportions, overwrite = TRUE)
