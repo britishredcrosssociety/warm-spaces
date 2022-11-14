ed1af46d931ee9dfcdfb0ced965477ae60da1e85
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-11-14 13:23:56 +0100

    Remove deprecated data

--- data-raw/imported-data/ltla-need.R
+++
@@ -1,5 +0,0 @@
-# Data from ad-hoc/analysis/cost-of-living
-ltla_need <-
-  readr::read_rds("data-raw/imported-data/warm-spaces-ltla-need.rds")
-
-usethis::use_data(ltla_need, overwrite = TRUE)
--- data-raw/regional-need.R
+++
@@ -1,5 +0,0 @@
-# Data from ad-hoc/analysis/cost-of-living
-regional_need <-
-  readr::read_rds("data-raw/imported-data/warm-spaces-regional-need.rds")
-
-usethis::use_data(regional_need, overwrite = TRUE)
