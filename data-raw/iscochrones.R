b7d18dbd5348f1a922aa22ef1169fba71e6e9b48
Matt Thomas <matthewgthomas@gmail.com>
2022-12-05 10:51:57 +0000

    Updated data

--- data-raw/iscochrones.R
+++ data-raw/iscochrones.R
@@ -1,5 +1,8 @@
+library(sf)
+
 # Data from ad-hoc/analysis/cost-of-living
 isochrones <-
-  readr::read_rds("data-raw/imported-data/warm-spaces-isochrones.rds")
+  readr::read_rds("data-raw/imported-data/warm-spaces-isochrones.rds") |> 
+  st_as_sf()
 
 usethis::use_data(isochrones, overwrite = TRUE)
--- data-raw/warm-spaces.R
+++ data-raw/warm-spaces.R
@@ -1,5 +1,5 @@
 # Data from ad-hoc/analysis/cost-of-living
 warm_spaces <-
-  readr::read_rds("data-raw/imported-data/all-warm-spaces-geocoded.rds")
+  readr::read_rds("data-raw/imported-data/warm-spaces-geocoded.rds")
 
 usethis::use_data(warm_spaces, overwrite = TRUE)
